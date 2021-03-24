9c21cd00d0143067bc4f57fc47206041fae0198e
Kaikai Liu <010796032@coe-hpc1.sjsuad.sjsu.edu>
2021-03-24 10:01:05 -0700

    Add Sign class

---
+++ DatasetTools/checkinfopkl.py
@@ -0,0 +1,17 @@
+import pickle
+
+filename = "/data/cmpe249-f20/WaymoKittitMulti/train0001/waymo_infos_val.pkl"
+infile = open(filename,'rb')
+new_dict = pickle.load(infile)
+infile.close()
+
+print(type(new_dict))
+for key, value in new_dict[0].items() :
+    #print (key, value)
+    print(key)
+    
+print(len(new_dict))
+for i in range(len(new_dict)):
+    print(new_dict[i]['annos']['name'])
+
+
