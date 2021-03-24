8c5fcadfe8648ff74849fc386203da6f9baefe77
Kaikai Liu <010796032@coe-hpc1.sjsuad.sjsu.edu>
2021-03-24 10:01:05 -0700

    Add Sign class

--- DatasetTools/Waymo2KittiAsync.py
+++ DatasetTools/Waymo2KittiAsync.py
@@ -56,7 +56,9 @@ class Waymo2KITTIAsync(object):
         self.filter_empty_3dboxes = True
         self.filter_no_label_zone_points = True
 
-        self.selected_waymo_classes = ['VEHICLE', 'PEDESTRIAN', 'CYCLIST']
+        #self.selected_waymo_classes = ['VEHICLE', 'PEDESTRIAN', 'CYCLIST']
+        self.selected_waymo_classes = ['VEHICLE', 'PEDESTRIAN', 'CYCLIST','SIGN']
+        print("selected_waymo_classes:", self.selected_waymo_classes)
 
         # Only data collected in specific locations will be converted
         # If set None, this filter is disabled
@@ -575,22 +577,23 @@ class Waymo2KITTIAsync(object):
 
 
 if __name__ == "__main__":
-    folders = ["training_0000","training_0001", "training_0002","training_0003","training_0004","training_0005","training_0006","training_0007","training_0008","training_0009", "training_0010", "training_0015", "training_0016", "training_0017","training_0018", "training_0019", "training_0020", "training_0021","training_0022","training_0023","training_0024","training_0025","training_0026","training_0027","training_0028","training_0029","training_0030","training_0031","validation_0000","validation_0001","validation_0002","validation_0003","validation_0004","validation_0005","validation_0006","validation_0007"]#["training_0001"]# ["training_0000", "training_0001"]
-    #folders = ["training_0000"]
+    #folders = ["training_0000","training_0001", "training_0002","training_0003","training_0004","training_0005","training_0006","training_0007","training_0008","training_0009", "training_0010", "training_0015", "training_0016", "training_0017","training_0018", "training_0019", "training_0020", "training_0021","training_0022","training_0023","training_0024","training_0025","training_0026","training_0027","training_0028","training_0029","training_0030","training_0031","validation_0000","validation_0001","validation_0002","validation_0003","validation_0004","validation_0005","validation_0006","validation_0007"]#["training_0001"]# ["training_0000", "training_0001"]
+    folders = ["training_0003"]
+    folder_name="4c_train0002"
     root_path="/data/cmpe249-f20/Waymo"
     out_dir="/data/cmpe249-f20/WaymoKittitMulti"
     data_files = [path for x in folders for path in glob(os.path.join(root_path, x, "*.tfrecord"))]
     print("totoal number of files:", len(data_files))
 
-    workers=16
+    workers=48 #16
     c_start = time.time()
     print(c_start)
-    save_dir = osp.join(out_dir, 'training', 'training_0000')
+    save_dir = osp.join(out_dir, folder_name, 'training')
     converter = Waymo2KITTIAsync(
             data_files,
             save_dir,
             workers=workers,
-            startingindex=560,
+            startingindex=0,
             test_mode=False)
     converter.concurrenttaskthread()#convert_multithread()#convertcoroutine()#concurrenttaskthread()#.convert()
     
