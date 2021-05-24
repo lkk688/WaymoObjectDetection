a7d13c7fe5599ca8cbbf7c36c020e5b56e1aabdc
Kaikai Liu <kaikai.liu@sjsu.edu>
2021-05-23 18:25:04 -0700

    change waymo frame2dict

--- DatasetTools/waymoframe2dict.py
+++ DatasetTools/waymoframe2dict.py
@@ -325,21 +325,7 @@ def extract_onesegment_toframe(fileidx, tfrecord_pathnames, step):
   
   return framesdict
 
-if __name__ == "__main__":
-  #test the above functions: convert a Frame proto into a dictionary
-  #convert_frame_to_dict
-
-  #folders = ["training_0000","training_0001", "training_0002","training_0003","training_0004","training_0005","training_0006","training_0007","training_0008","training_0009", "training_0010", "training_0015", "training_0016", "training_0017","training_0018", "training_0019", "training_0020", "training_0021","training_0022","training_0023","training_0024","training_0025","training_0026","training_0027","training_0028","training_0029","training_0030","training_0031","validation_0000","validation_0001","validation_0002","validation_0003","validation_0004","validation_0005","validation_0006","validation_0007"]#["training_0001"]# ["training_0000", "training_0001"]
-  folders = ["training_0000"]
-  root_path="/data/cmpe249-f20/Waymo"
-  out_dir="/data/cmpe249-f20/WaymoKittitMulti/dict_train0"
-  data_files = [path for x in folders for path in glob(os.path.join(root_path, x, "*.tfrecord"))]
-  print("totoal number of files:", len(data_files))#886
-
-  c_start = time.time()
-  print(c_start)
-  fileidx = 1
-  step = 10
+def saveonedictfile(data_files, fileidx, step, out_dir):
   framesdict = extract_onesegment_toframe(fileidx, data_files, step)
   num_frames = len(framesdict)
   print(num_frames)
@@ -366,6 +352,36 @@ if __name__ == "__main__":
   filename=str(fileidx)+'_'+'step'+str(step)+'_'+context_name+'.npz'
   np.savez_compressed(out_dir / filename, Final_array)#Execution time: 579.5185077190399, 7G
   print(f"Finished np save, Execution time: { time.time() - second_time }") 
+
+if __name__ == "__main__":
+  #test the above functions: convert a Frame proto into a dictionary
+  #convert_frame_to_dict
+
+  #folders = ["training_0000","training_0001", "training_0002","training_0003","training_0004","training_0005","training_0006","training_0007","training_0008","training_0009", "training_0010", "training_0015", "training_0016", "training_0017","training_0018", "training_0019", "training_0020", "training_0021","training_0022","training_0023","training_0024","training_0025","training_0026","training_0027","training_0028","training_0029","training_0030","training_0031","validation_0000","validation_0001","validation_0002","validation_0003","validation_0004","validation_0005","validation_0006","validation_0007"]#["training_0001"]# ["training_0000", "training_0001"]
+  folders = ["training_0000"]
+  root_path="/data/cmpe249-f20/Waymo"
+  out_dir="/data/cmpe249-f20/WaymoKittitMulti/dict_train0"
+  data_files = [path for x in folders for path in glob(os.path.join(root_path, x, "*.tfrecord"))]
+  print("totoal number of files:", len(data_files))#886
+
+  c_start = time.time()
+  print(c_start)
+  fileidx = 1
+  step = 10
+  #save a single dict file
+  #saveonedictfile(data_files, fileidx, step, out_dir)
+
+  #save validation folders to dict files
+  folders = ["validation_0000","validation_0001","validation_0002","validation_0003","validation_0004","validation_0005","validation_0006","validation_0007"]
+  root_path="/data/cmpe249-f20/Waymo"
+  out_dir="/data/cmpe249-f20/WaymoKittitMulti/validationalldicts"
+  data_files = [path for x in folders for path in glob(os.path.join(root_path, x, "*.tfrecord"))]
+  print("totoal number of files:", len(data_files))#886
+  step=1
+  for fileidx in range(len(data_files)):
+    saveonedictfile(data_files, fileidx, step, out_dir)
+  print("finished")
+  
 
     
 
